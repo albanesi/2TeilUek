2aaa10a760f61c5c92fc55a04125735ba38fc72b
albanesi <alban.selimi@edu.tbz.ch>
2020-04-09 09:14:13 +0200

    the spring boot project pushed to origin

---
+++ lambdaprojects/src/test/java/ch/lambdaCourse/lambdaprojects/LambdaprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.lambdaCourse.lambdaprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
